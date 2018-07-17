efefdb7370a455173dfe58858170d905424aaf60
MayaNishita <mayaxxx1101@gmail.com>
2018-07-17 10:51:38 +0900

    最初のコミット

---
+++ wp-content/themes/fsvbasic_child/sidebar-footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage FSVBASIC
+ * @since FSVBASIC 1.0
+ */
+?>
+
+<div id="sub" class="footer-widget-area" role="complementary">
+
+<div class="clear"></div>
+
+</div><!-- #sub -->
